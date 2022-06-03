f46caf9be0f57ce3d8135a364d481a112941d741
nicopcat <tianfang07@outlook.com>
2022-06-03 14:04:31 +0800

    first commit

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,17 @@
-module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset'
-  ]
+module.exports = function (api) {
+  api.cache(true);
+
+  const presets = [["es2015", { "modules": false }]];
+  const plugins = [[
+    "component",
+    {
+      "libraryName": "element-ui",
+      "styleLibraryName": "theme-chalk"
+    }
+  ]];
+
+  return {
+    presets,
+    plugins
+  };
 }
